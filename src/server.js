fb47ea7bc822d2d015023ad52d33a1babe704ecb
layraneleao <contato.layrane@gmail.com>
2024-04-17 04:39:35 -0300

    Alteração no pacote package

--- src/server.js
+++ src/server.js
@@ -6,10 +6,11 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const server = express();
+
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
 
-server.use('/api', routes);
+server.use('/', routes);
 
 server.listen(process.env.PORT, ()=>{
     console.log(`Servidor rodando em http://localhost:${process.env.PORT}`);
